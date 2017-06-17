c52ab257bd3424f3726b167d1d9b813ebbf8dd3d
root <root@salt.hackafe.net>
2017-06-17 19:40:56 -0400

    Fixed paths

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ killgroup(){
   kill 0
 }
 
-pxed -g 10.2.0.1 -v -D ens18 /root/pxe/tftproot/ lpxelinux.0 &
+pxed -g 10.2.0.1 -v -D ens18 `readlink -e ./tftproot` lpxelinux.0 &
 cd httproot; python -m SimpleHTTPServer 8080 &
 
 wait
