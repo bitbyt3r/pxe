76ff3d2e292705b3e19d4360f956cfc0fe80375c
root <root@salt.hackafe.net>
2017-06-17 01:49:14 -0400

    Initial config

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+echo "Starting PXE Server"
+
+firewall-cmd --add-service=tftp --permanent
+firewall-cmd --add-port=8080/tcp --permanent
+firewall-cmd --add-service=dhcp --permanent
+firewall-cmd --reload
+
+trap killgroup SIGINT
+
+killgroup(){
+  echo Killing...
+  kill 0
+}
+
+pxed -g 10.2.0.1 -v -D ens18 /root/pxe/tftproot/ lpxelinux.0 &
+cd httproot; python -m SimpleHTTPServer 8080 &
+
+wait
